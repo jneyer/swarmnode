b1a94273e1f2fc0a0fac4e4e5c7148984ca1c7bc
John Neyer <jneyer@captechconsulting.com>
2017-04-18 16:02:44 -0400

    Adding hostname

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ var os = require('os');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\nYour address: "+ myIp());
+  response.end("Hello World\nAddresses: " + myIp() + "/nHost: " + os.hostname());
 });
 
 function myIp() {
    var interfaces = os.networkInterfaces();
-var addresses = [];
-for (var k in interfaces) {
-    for (var k2 in interfaces[k]) {
-        var address = interfaces[k][k2];
-        if (address.family === 'IPv4' && !address.internal) {
-            addresses.push(address.address);
-        }
-    }
-}
- return addresses;
+   var addresses = [];
+   for (var k in interfaces) {
+      for (var k2 in interfaces[k]) {
+         var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+               addresses.push(address.address);
+         }
+      }
+   }
+   return addresses;
 }
 
 // Listen on port 8000, IP defaults to 127.0.0.1
