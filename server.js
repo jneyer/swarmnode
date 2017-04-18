c690d30c4a7c5331eeaf10496597e26d20c2e3d5
John Neyer <jneyer@captechconsulting.com>
2017-04-18 15:28:35 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+// Load the http module to create an http server.
+var http = require('http');
+var os = require('os');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\nYour address: "+ myIp());
+});
+
+function myIp() {
+   var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+ return addresses;
+}
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8080);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8080/");
